ebc3bcb1a14b92e61b89791a55cf1fd009e08f43
Tai <taiwoodley@gmail.com>
2025-04-26 21:39:50 +1000

    Initial commit

---
+++ assignment3/assignment3/assignment3App.swift
@@ -0,0 +1,17 @@
+//
+//  assignment3App.swift
+//  assignment3
+//
+//  Created by Tai Woodley on 26/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct assignment3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
