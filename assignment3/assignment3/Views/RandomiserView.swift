a0330ae8825fe0048e5cf1a2d967a7bbd5b9119e
Tai <taiwoodley@gmail.com>
2025-04-27 17:01:22 +1000

    Create initial tab bar and views

---
+++ assignment3/assignment3/Views/RandomiserView.swift
@@ -0,0 +1,18 @@
+//
+//  RandomiserView.swift
+//  assignment3
+//
+//  Created by Tai Woodley on 27/4/2025.
+//
+
+import SwiftUI
+
+struct RandomiserView: View {
+    var body: some View {
+        Text("Randomiser")
+    }
+}
+
+#Preview {
+    RandomiserView()
+}
---
+++ assignment3/assignment3/Views/TeaListView.swift
@@ -0,0 +1,18 @@
+//
+//  TeaListView.swift
+//  assignment3
+//
+//  Created by Tai Woodley on 27/4/2025.
+//
+
+import SwiftUI
+
+struct TeaListView: View {
+    var body: some View {
+        Text("Teas")
+    }
+}
+
+#Preview {
+    TeaListView()
+}
