32c77c3566e6e45049b37fcf585559ed6eb77e45
Tai <taiwoodley@gmail.com>
2025-04-28 19:00:58 +1000

    Implement initial timer behaviour

--- assignment3/Models/TeaDb.swift
+++ assignment3/Models/TeaDb.swift
@@ -20,7 +20,8 @@ class TeaDb: ObservableObject {
         let tea2 = TeaModel(id: 1, name: "Spices", type: .black, waterAmount: 250, waterTemp: 100, time: 180, url: "https://www.lupicia.com.au/22/black-tea-flavoured/195/spices-chai-style")
         let tea3 = TeaModel(id: 2, name: "Sencha Chiran", type: .green, waterAmount: 250, waterTemp: 60, time: 60, url: "https://www.lupicia.com.au/17/japanese-green-tea/75/sencha-chiran")
         let tea4 = TeaModel(id: 3, name: "Momo Oolong", type: .oolong, waterAmount: 250, waterTemp: 100, time: 180, url: "https://www.lupicia.com.au/178/momo-oolong-collection/99/momo-oolong-super-grade")
+        let tea5 = TeaModel(id: 4, name: "Speed tea", type: .green, waterAmount: 0, waterTemp: 25, time: 5, url: "https://example.com")
         
-        return [tea1, tea2, tea3, tea4]
+        return [tea1, tea2, tea3, tea4, tea5]
     }
 }
--- assignment3/Views/TimerView.swift
+++ assignment3/Views/TimerView.swift
@@ -9,19 +9,52 @@ import SwiftUI
 
 struct TimerView: View {
     @StateObject var tea: TeaModel
+    
+    @State var remainingTime: Int = -1
+    @State var showTimer: Bool = false
+    @State var timerMessage = ""
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common)
+    let dotSuffix: [String] = ["", ".", "..", "..."]
+    
     var body: some View {
         VStack {
             Text(tea.name).font(.title)
             Spacer()
-            Text("Water temp: \(tea.waterTemp) ºC")
-            Text("Water amount: \(tea.waterAmount) mL")
-            Text("Time: \(tea.time) seconds")
-            Spacer()
-        }
 
+            if showTimer || remainingTime > 1 {
+                Text("Remaining time: \(remainingTime/60):\(String(format: "%02d", remainingTime%60))").padding()
+            } else {
+                Text("Water temp: \(tea.waterTemp) ºC")
+                Text("Water amount: \(tea.waterAmount) mL")
+                Text("Time: \(tea.time) seconds")
+                Button(action: beginTimer) {
+                    Text("Begin").padding()
+                }
+            }
+            Text(timerMessage)
+            Spacer()
+        }.onReceive(timer, perform: {_ in onTimer()})
+    }
+    
+    func beginTimer() {
+        remainingTime = tea.time
+        showTimer = true
+        _ = timer.connect()
+        timerMessage = "Steeping"
+    }
+    
+    func onTimer() {
+        if remainingTime != 0 {
+            remainingTime -= 1
+            timerMessage = "Steeping" + dotSuffix[(tea.time - remainingTime) % 4]
+        } else {
+            timerMessage = "Your tea is ready. Enjoy!"
+            showTimer = false
+        }
     }
 }
 
 #Preview {
-    TimerView(tea: TeaDb().teas[0])
+    TimerView(tea: TeaDb().teas[4])
 }
