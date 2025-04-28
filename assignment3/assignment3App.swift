28b37a5c389058eb9915c22d803be8694717478c
Tai <taiwoodley@gmail.com>
2025-04-28 22:58:17 +1000

    Fix notifications not showing while app is in the foreground

--- assignment3/assignment3App.swift
+++ assignment3/assignment3App.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 @main
 struct assignment3App: App {
+    @UIApplicationDelegateAdaptor(NotificationDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
