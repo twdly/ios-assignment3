9b97fe461993d32257dc7efe940bf22e340c4a15
Tai <taiwoodley@gmail.com>
2025-05-01 22:04:14 +1000

    Implement post requests to review server

--- assignment3/Views/WriteReviewView.swift
+++ assignment3/Views/WriteReviewView.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct WriteReviewView: View {
     @StateObject var tea: TeaModel
-    @State var rating: Int = 1
+    @State var rating: Int = 0
     @State var reviewText: String = ""
+    @State var errorMessage: String = ""
     
     var body: some View {
         VStack {
@@ -24,14 +25,31 @@ struct WriteReviewView: View {
                 Text("/ 10")
             }
             TextField("Share your thoughts...", text: $reviewText).frame(width: 300, height: 150, alignment: .top).border(.gray)
-            Button(action: submit) {
+            Button {
+                Task {
+                    await submit()
+                }
+            } label: {
                 Text("Submit")
             }.padding()
+            Text(errorMessage).foregroundStyle(.red)
         }.navigationTitle("Review \(tea.name)")
     }
     
-    func submit() {
+    func submit() async {
+        let review = ReviewModel(id: -1, teaName: tea.name, rating: rating + 1, message: reviewText, url: tea.url ?? "")
         
+        // This URL is a web server used to store and read reviews in json format
+        // https://github.com/twdly/tea-reviews
+        let url = URL(string: "https://teareview-fuaygvbagwfegda8.australiaeast-01.azurewebsites.net/")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        do {
+            _ = try await URLSession.shared.upload(for: request, from: JSONEncoder().encode(review))
+        } catch {
+            errorMessage = "Something went wrong, please try again later"
+        }
     }
 }
 
