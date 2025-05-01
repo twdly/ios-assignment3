33ab833eea1431c26bdcb40c8b7795b12ebdf713
Tai <taiwoodley@gmail.com>
2025-05-01 20:59:00 +1000

    Create WriteReviewView and navigation

---
+++ assignment3/Views/WriteReviewView.swift
@@ -0,0 +1,20 @@
+//
+//  WriteReviewView.swift
+//  assignment3
+//
+//  Created by Tai Woodley on 1/5/2025.
+//
+
+import SwiftUI
+
+struct WriteReviewView: View {
+    
+    
+    var body: some View {
+        Text("Write review view")
+    }
+}
+
+#Preview {
+    WriteReviewView()
+}
