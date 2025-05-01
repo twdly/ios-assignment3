d98d37f3bf79e272c56ab55f5814952e7b665803
Tai <taiwoodley@gmail.com>
2025-05-01 22:18:27 +1000

    Implement reading reviews from server

--- assignment3/Views/ReviewView.swift
+++ assignment3/Views/ReviewView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct ReviewView: View {
+    @State var reviews: [ReviewModel] = []
+    @State var errorMessage: String = ""
+    
     var body: some View {
-        Text("Reviews")
+        VStack {
+            if reviews.isEmpty {
+                Text(errorMessage.isEmpty ? "No reviews could be found" : errorMessage)
+            } else {
+                List {
+                    ForEach(reviews) { review in
+                        VStack {
+                            Text(review.teaName).font(.title2)
+                            Text("\(review.rating) / 10")
+                            Text(review.message)
+                        }
+                    }
+                }
+            }
+        }.onAppear(perform: {
+            Task {
+                await loadReviews()
+            }
+        })
+    }
+    
+    func loadReviews() async {
+        let url = URL(string: "https://teareview-fuaygvbagwfegda8.australiaeast-01.azurewebsites.net/")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "GET"
+        do {
+            let result = try await URLSession.shared.data(for: request)
+            reviews = try JSONDecoder().decode([ReviewModel].self, from: result.0)
+        } catch {
+            errorMessage = "Something went wrong, please try again later"
+        }
     }
 }
 
