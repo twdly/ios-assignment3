11302a99e745bcced60e764c8da61496762fdcf6
Tai <taiwoodley@gmail.com>
2025-05-06 18:22:59 +1000

    Create timerlistview and add it to tabs

---
+++ assignment3/Views/TimerListView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerListView.swift
+//  assignment3
+//
+//  Created by Tai Woodley on 6/5/2025.
+//
+
+import SwiftUI
+
+struct TimerListView: View {    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerListView()
+}
